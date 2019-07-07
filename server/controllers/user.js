9e02ac1768be2b954ce23d749a71a7151c9951dd
kazeem <kazeem0825@gmail.com>
2019-07-07 20:17:21 +0100

    reset token route added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,26 @@
 class UserController {
   signUp() {}
-  login() {}
+
+  async login(req, res) {
+    //find user by email
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) res.status(401).send({ error: 'Invalid username/password' });
+
+    //compare the passwords
+    const password = bcrypt.compare(user.password, req.password);
+    if (!password) return res.status(400).send('Invalid email/password');
+
+    //generate token
+    const token = user.generateToken();
+    res.send({ token });
+  }
+
+  async resetToken(req, res){
+    const user = await User.findOne({ _id: req.user.id });
+    mailer.sendMail(user.email, user.firstName, token, mailOptions);
+
+    res.send('Please check your email and verify your account');
+  }
 }
 
 module.exports = new UserController();
