0d4a2e19656ef97356a36102f0906a27df900c1e
Grace Frank <frankgracey@gmail.com>
2019-07-09 14:24:16 +0100

    add auth/signup route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { User } = require('../db/db');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
@@ -10,17 +10,17 @@ const router = express.Router();
 // });
 
 router.post('/login', async (req, res) => {
+  // find user by email
+  let user = await User.findOne({ where: { email: req.body.email } });
+  if (!user) res.status(401).send({ error: 'Invalid username/password' });
 
-    // find user by email
-    let user = await User.findOne({where: { email: req.body.email}});
-    if(!user) res.status(401).send({error: "Invalid username/password"});
+  //compare the passwords
+  const password = bcrypt.compare(user.password, req.password);
+  if (!password) return res.status(400).send('Invalid email/password');
 
-    //compare the passwords
-    const password = bcrypt.compare(user.password, req.password);
-    if (!password) return res.status(400).send('Invalid email/password');
+  res.send(user);
+});
 
-    res.send(user)
-   
-})
+
 
 module.exports = router;
